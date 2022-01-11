24178a4c523d9206a8d1f72b0965b990cb06be16
Aran <aran@ermarian.net>
2022-01-11 06:27:19 +0100

    better UI

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from core import Solver
+#!/usr/bin/env python
+
+from core import Solver, check_guess
 import argparse
 
-solver = Solver()
+def main():
+    solver = Solver()
 
-parser = argparse.ArgumentParser(description='Mastermind')
-parser.add_argument('solution', metavar='s', type=str, nargs='?',
-                    help='Guess against a pre-selected solution')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Mastermind')
+    parser.add_argument('solution', metavar='s', type=str, nargs='?',
+                        help='Guess against a pre-selected solution')
+    args = parser.parse_args()
 
-if args.solution:
-    print(solver.solve(args.solution))
-else:
-    solver.solve()
+    try:
+        if args.solution:
+            for guess in solver.solve(args.solution):
+                x = "".join(map(str, check_guess(guess, args.solution)))
+                print(f"{guess}: {x}")
+        else:
+            print("2 = green, 1 = yellow, 0 = grey")
+            solver.solve()
+    except ValueError as e:
+        print(e)
+
+main()
